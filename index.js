d861971c8d5d145f9421e00aca7bf86a566b791e
LBenzahia <lakhdar.benzahia@gmail.com>
2017-09-24 23:20:23 +0100

    bump version 1.1.2

---
+++ index.js
@@ -0,0 +1,12 @@
+
+module.exports.fonts  = {
+        Roboto: {
+            normal: './node_modules/pdf-fonts/fonts/Roboto-Regular.ttf',
+            bold: './node_modules/pdf-fonts/fonts/Roboto-Medium.ttf',
+            italics: './node_modules/pdf-fonts/fonts/Roboto-Italic.ttf',
+            bolditalics: './node_modules/pdf-fonts/fonts/Roboto-Italic.ttf'
+        }
+};
+
+
+
